c2378bf04cc35a1aa8ffb29b8403537e8542d144
Juan O <juanormaechea97@gmail.com>
2025-04-06 18:46:01 +0200

    primera subida, conexion con la api montada

---
+++ src/etl.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+URL = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/hlth_silc_13?geo=ES&sex=T&age=Y25-34&format=JSON"
+
+def download_data():
+    print("Descargando datos desde Eurostat...")
+
+    try:
+        response = requests.get(URL, timeout=10)  # Espera máximo 10 segundos
+        response.raise_for_status()  # Lanza excepción si el status no es 200
+
+        data = response.json()
+        with open("data/eurostat_raw.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=2)
+        print("✅ Datos guardados correctamente en data/eurostat_raw.json")
+
+    except requests.exceptions.Timeout:
+        print("⏱️ La solicitud ha tardado demasiado y fue cancelada (timeout).")
+
+    except requests.exceptions.RequestException as e:
+        print(f"❌ Error al descargar datos: {e}")
+
+if __name__ == "__main__":
+    download_data()
