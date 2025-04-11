74b39a06a5d4427a07fa319d0b4da013b46bd38a
Juan <jojoji54@gmail.com>
2025-04-11 23:04:53 +0200

    me faltaba el enviroments 2

--- src/eda.py
+++
@@ -1,6 +0,0 @@
-def mostrar_estadisticas_basicas():
-    print("Este módulo podría contener funciones para análisis exploratorio,")
-    print("- Conteo de valores únicos por columna")
-    print("- Porcentaje de valores nulos")
-    print("- Distribuciones básicas")
-    print("- Detección de duplicados o valores atípicos")
